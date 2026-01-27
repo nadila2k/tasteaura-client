c52219f0fcb5582809773bc06b17cd6710628e58
nadila2k <nadilanawod@gmail.com>
2026-01-27 13:04:19 +0530

    menu page done

--- components/menu/Menu.js
+++ components/menu/Menu.js
@@ -6,22 +6,32 @@ import CategorieGrid from "./CategorieGrid";
 import MenuitemGrid from "./MenuitemGrid";
 
 export default function Menu({ categories = [], menuItems = [] }) {
-
-  console.log(menuItems)
+  console.log(menuItems);
   const [selectedCategoryId, setSelectedCategoryId] = useState(
     categories.length > 0 ? categories[0].id : null,
   );
+
+  const [search, setSearch] = useState("");
 
   const handleCategorySelect = (categoryId) => {
     setSelectedCategoryId(categoryId);
   };
 
- const filteredMenuItems =
-  selectedCategoryId === 1
-    ? menuItems // show all food
-    : menuItems.filter(
-        (item) => item.category?.id === selectedCategoryId
-      );
+  const categoryFiltered =
+    selectedCategoryId === 1
+      ? menuItems
+      : menuItems.filter((item) => item.category?.id === selectedCategoryId);
+
+
+  const filteredMenuItems = categoryFiltered.filter((item) => {
+    const query = search.toLowerCase();
+
+    return (
+      item.name.toLowerCase().includes(query) ||
+      item.category?.name.toLowerCase().includes(query)
+    );
+  });
+
   return (
     <div className={styles.container}>
       <div className={styles.flexContainer}>
@@ -41,6 +51,15 @@ export default function Menu({ categories = [], menuItems = [] }) {
           {categories.find((cat) => cat.id === selectedCategoryId)?.name ||
             "Select a category"}
         </h1>
+        <div className={styles.searchWrapper}>
+          <input
+            type="text"
+            placeholder="Search by item or category..."
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+            className={styles.searchInput}
+          />
+        </div>
         <div className={styles.menuitem}>
           {filteredMenuItems.map((item) => (
             <MenuitemGrid key={item.id} menuItem={item} />
